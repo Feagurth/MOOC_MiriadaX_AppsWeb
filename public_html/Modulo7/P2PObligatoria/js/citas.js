a0c1c73efc596c369fc940ac97d0cb564c02fd08
Feagurth <Feagurth@gmail.com>
2016-05-08 21:59:42 +0200

    Modulo 7 P2PObligatoria: Corrección de tipos.

--- public_html/Modulo7/P2PObligatoria/js/citas.js
+++ public_html/Modulo7/P2PObligatoria/js/citas.js
@@ -4,7 +4,7 @@
  */
 var galeria = [
     {persona: "Buddha - बुद्धा",
-        frase: "En la confrontación entre el arrollo y la roca, el arrollo siempre ganará, no por la fuerza, sino por la persistencia.",
+        frase: "En la confrontación entre el arroyo y la roca, el arrollo siempre ganará, no por la fuerza, sino por la persistencia.",
         foto: "http://www.imagexia.com/img/Cara-de-Buda.jpg"
     },
     {persona: "Khalil Gibran - جبران خليل جبران بن ميخائل بن سعد",
